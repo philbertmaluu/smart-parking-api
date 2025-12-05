ab923dba3666311da4580293a2bb3e4aa9f21bd6
Barakael Lucas <barakaellucas2019@gmail.com>
2025-12-05 15:31:14 +0300

    feat: Add VehicleBodyTypePriceSeeder to database seeding process

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             VehicleBodyTypeSeeder::class,
             PaymentTypeSeeder::class,
+            VehicleBodyTypePriceSeeder::class,
         ]);
 
         $this->command->info('All seeders completed successfully!');
